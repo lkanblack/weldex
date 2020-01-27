496a247db3c6b3b7143e4a2a8277a5fa937fdfa9
Andrei <andrei@test.ee>
2020-01-27 07:21:27 +0000

    base-html

--- post/views.py
+++ post/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'post/main_page.html', {})
+    posts = Post.objects.all()
+    return render(request, 'post/main_page.html', {'posts': posts})
