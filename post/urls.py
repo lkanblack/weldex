938ba9a3b923f9717765212697473cf9174c8988
andre <andrei@test.ee>
2020-02-25 10:42:03 +0000

    lang header

--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^gallery/', views.gallery,  name='gallery'),
+    path('gallery/', views.gallery,  name='gallery'),
     path('rus/', views.rus,  name='rus'),
     path('eng/', views.eng,  name='eng'),
 ]
